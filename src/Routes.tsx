e0fa0c8b8875c561a033235b113bb6daf2a7f72b
cssahil98 <cssahil98@outlook.com>
2023-01-05 23:13:58 +0530

    "feat: added typescript format"

---
+++ src/Routes.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+// import React, { Fragment } from "react";
+// import {Route, RouteProps} from "react-router-dom";
+
+// import { Home } from "./components/Portfolio/Home";
+
+// export const ROUTE_LINKS = {
+//   HOME: "/",
+// };
+
+// export const ROUTES: RouteProps[] = [
+//   {path: ROUTE_LINKS.HOME, component: Home, exact: true},
+// ];
+
+// export function Routes() {
+//   return(
+//     <Fragment>
+//       {ROUTES.map((route, i) => {
+//         const key = getKey({path: route.path, index: i});
+//         return <Route {...route} key={key} />
+//       })}
+//     </Fragment>
+//   )
+// }
+
+// function getKey({path, index}: {path: RouteProps["path"]; index: number }) {
+//   if (typeof path !== "string") return index;
+//   if (path === "") return index;
+//   return path;
+// }
+export{}
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
+import App from './App';
+import { store } from './redux/store';
+import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
